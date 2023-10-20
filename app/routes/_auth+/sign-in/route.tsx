d0ba2bb22eda040b3a7a7ded6810f6d0b68f132d
Carlos Umaña <dev.carlos.umana@gmail.com>
2023-10-20 10:34:41 -0500

    [feat]: create sign in form

--- app/routes/_auth+/sign-in/route.tsx
+++ app/routes/_auth+/sign-in/route.tsx
@@ -1,9 +1,25 @@
+import { Button } from "~/components/button";
+import { TextInput } from "~/components/text-input";
 import { SignFormWrapper } from "~/components/sign-form-wrapper";
+import { Link } from "@remix-run/react";
 
 export default function SignInRoute() {
   return (
     <SignFormWrapper title="Sign In" caption="Welcome back! Fill the form and sign in">
-      <h1>Sign In</h1>
+      <form>
+        <div className="mb-8 space-y-5">
+          <TextInput label="Email" name="email" type="email" />
+
+          <div className="flex flex-col space-y-2">
+            <TextInput label="Password" name="password" type="password" />
+            <Link to="/recover-password" className="text-right text-xs text-gray-400">
+              Forgot my password
+            </Link>
+          </div>
+        </div>
+
+        <Button width="full">Sign In</Button>
+      </form>
     </SignFormWrapper>
   );
 }
